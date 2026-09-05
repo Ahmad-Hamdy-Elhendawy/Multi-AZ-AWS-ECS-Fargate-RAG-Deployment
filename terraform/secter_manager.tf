d5b91cc2837ec5408c77b01c997add8ca6d0c7b2
Ahmad-Hamdy-Elhendawy <ahmad.hamdy.elhendawy@gmail.com>
2026-09-05 17:04:05 +0300

    Revert "exp w secrets"
    This reverts commit 5bdd54cb5922d9296d95209edf489b3de17a6b67.

--- terraform/secter_manager.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_secretsmanager_secret" "groq_api_key" {
-  name = "groq-api-key"
-}
-
-resource "aws_secretsmanager_secret_version" "groq_api_key" {
-  secret_id     = aws_secretsmanager_secret.groq_api_key.id
-  secret_string = var.GROQ_API_KEY
-}
